0ee56ce8332414030939f29a2acbe9698ffdbdef
BEES_KNEES <95536089+boscoez@users.noreply.github.com>
2022-09-26 23:31:52 -0700

    Update CS380_Lab0.java

--- src/packagetest/CS380_Lab0.java
+++ src/packagetest/CS380_Lab0.java
@@ -6,14 +6,14 @@ public class CS380_Lab0 {
 		// TODO Auto-generated method stub
 		System.out.println("First Lab CS 380");
 		int number = 12345, reverse = 0; 
-		System.out.println("Given number is: "+number);
+		System.out.println("Given number: "+number);
 		while(number != 0)   
 		{  
 		int remainder = number % 10;  
 		reverse = reverse * 10 + remainder;  
 		number = number/10;  
 		}  
-		System.out.println("The reverse of the given number is: " + reverse);  
+		System.out.println("The reverse of given number: " + reverse);  
 		}  
 
 	}

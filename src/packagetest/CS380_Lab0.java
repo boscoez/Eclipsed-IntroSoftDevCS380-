879162d4c01e6c2f077b56a198ba78a870c59216
BEES_KNEES <95536089+boscoez@users.noreply.github.com>
2022-09-28 20:39:29 -0700

    Update CS380_Lab0.java

--- src/packagetest/CS380_Lab0.java
+++ src/packagetest/CS380_Lab0.java
@@ -4,16 +4,12 @@ public class CS380_Lab0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First Lab CS 380");				// initialize title of exercise
+		System.out.println("First Lab CS 380");					// initialize title of exercise
 		
-		int number = 12345, reverse = 0; 				// initialize number sequence
-		System.out.println("Given number: "+ number);			// printing the original number
+		String string = "12345"; 						// initialize number sequence
+		System.out.println("Given number: "+string);				// printing the original number
 		
-		while(number != 0){  						// while loop for all numbers not zero
-		int remainder = number % 10;  					// initialize remainder 		
-		reverse = reverse * 10 + remainder;  				// reversing the numerical sequence
-		number = number/10;  						// ordering reversed sequence
-		}
-		System.out.println("The reverse of given number: " + reverse);  // printing reversed number sequence
+		String reverse = new StringBuffer(string).reverse().toString();		// ordering reversed sequence
+		System.out.println("The reverse of given number: " +reverse);  		// printing reversed number sequence
 		}
 	}
